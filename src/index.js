6cb13eeb93e0266a5a79522ec5cce32be1e7091b
Gleb Billig <gleb.billig@gmail.com>
2020-05-19 16:32:14 -0400

    Add CSS rules for tiles and tile holder

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 
 function Tile(props) {
   return (
-    <span className="tile">
-      {props.letter}
-    </span>
+    <div className="tile">
+      <span className="letter">
+        {props.letter}
+      </span>
+    </div>
   )
 }
 
@@ -16,9 +18,9 @@ function PlayerTiles(props) {
   })
 
   return (
-    <span className="playerTiles">
+    <div className="tile-holder">
       {tiles}
-    </span>
+    </div>
   )
 }
 
@@ -36,7 +38,7 @@ class Game extends React.Component {
     let letterDistribution = {"A": 9, "B": 2, "C": 2, "D": 4, "E": 12, "F": 2, "G": 3, "H": 2, "I": 9, "J": 1, "K": 1, "L": 4, "M": 2, "N": 6, "O": 8, "P": 2, "Q": 1, "R": 6, "S": 4, "T": 6, "U": 4, "V": 2, "W": 2, "X": 1, "Y": 2, "Z": 1, " ": 2}
     this.state = {
       remainingTiles: ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z", " ", " "],
-      players: [{tiles: ['A']}, {tiles: []}, {tiles: ['B', 'C']}, {tiles: []}],
+      players: [{tiles: ['G', 'E', 'R', 'A', 'L']}, {tiles: ['<', '3']}, {tiles: ['G', 'L', 'E', 'B']}, {tiles: []}],
     };
   }
 
